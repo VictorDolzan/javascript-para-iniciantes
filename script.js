40223af39790dcf07987cec3fdc89da693f5c1ed
VictorDolsan <victorddf5@gmail.com>
2023-07-11 19:45:17 -0300

    Feito uma animação na hora do scroll do site

--- script.js
+++ script.js
@@ -60,6 +60,25 @@ function initSmoothScroll() {
     })
 }
 
+function animaScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+        function initScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0
+                if (isSectionVisible) {
+                    section.classList.add('ativo');
+                }
+            })
+        }
+        initScroll();
+        window.addEventListener('scroll', initScroll);
+    }
+}
+
 initTabNav();
 initAccordion();
 initSmoothScroll();
+animaScroll();
