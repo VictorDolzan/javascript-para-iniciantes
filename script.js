e6a22748bcff6d423d2e122d4494599a2687ae38
VictorDolsan <victorddf5@gmail.com>
2023-07-11 19:05:46 -0300

    Feito SmoothScroll na página

--- script.js
+++ script.js
@@ -37,5 +37,29 @@ function initAccordion() {
     }
 }
 
+function initSmoothScroll() {
+
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+    linksInternos.forEach((item) => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            const href = event.currentTarget.getAttribute('href');
+            const section = document.querySelector(href);
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+
+            /* FORMA ALTERNATIVA
+            let topo = section.offsetTop;
+            window.scrollTo({
+                top: topo,
+                behavior: 'smooth'
+            });*/
+        })
+    })
+}
+
 initTabNav();
 initAccordion();
+initSmoothScroll();
