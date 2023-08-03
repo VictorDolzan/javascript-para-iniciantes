020d71f20a347e4b803fe1ef83f124aa17d110b5
VictorDolsan <victorddf5@gmail.com>
2023-08-03 20:37:01 -0300

    Criada a separação dos arquivos css Criado o arquivo css modal.css Criado HTML do modal em index.html Criada a lógica de js em modal.js

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ import initSmoothScroll from './modules/scroll-suave.js';
 import animaScroll from './modules/scroll-animacao.js';
 import initTabNav from './modules/init-tab-nav.js';
 import initAccordion from './modules/init-accordion.js';
-debugger
+import initModal from './modules/modal.js'
 
 initSmoothScroll();
 animaScroll();
 initTabNav();
 initAccordion();
+initModal();
