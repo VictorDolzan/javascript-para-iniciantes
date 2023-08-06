ac07c0da70da82e73d323bbf2b4f7172c511a6f1
VictorDolsan <victorddf5@gmail.com>
2023-08-06 19:07:56 -0300

    Criado método outsideClick.js

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,16 +1,18 @@
-export default function initDropdownMenu(){
+import outsideClick from "./outsideClick.js";
+export default function initDropdownMenu() {
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-}
+    dropdownMenus.forEach(menu => {
+        ['touchstart', 'click'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick);
+        });
+    });
 
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-
-dropdownMenus.forEach(menu => {
-   ['touchstart', 'click'].forEach(userEvent => {
-       menu.addEventListener(userEvent, handleClick);
-   });
-});
-
-function handleClick(event){
-    event.preventDefault();
-    this.classList.toggle('active');
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add('active');
+        outsideClick(this, ['touchstart', 'click'], () => {
+            this.classList.remove('active');
+        });
+    }
 }
