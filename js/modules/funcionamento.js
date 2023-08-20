d3a342691316cea94f2356f4aaf99e0677a74d5a
VictorDolsan <victorddf5@gmail.com>
2023-08-19 21:13:47 -0300

    Subindo alteração de funcionamento.js

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,20 +1,18 @@
-export default function initFuncionamento(){
+export default function initFuncionamento() {
+    const funcionamento = document.querySelector('[data-semana]');
+    const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+    const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
 
-}
+    const dataAgora = new Date();
+    const diaAgora = dataAgora.getDay();
+    const horarioAgora = dataAgora.getHours();
+    const estaAberto = diasSemana.indexOf(diaAgora) !== -1;
 
-const funcionamento = document.querySelector('[data-semana]');
-const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
-const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
+    let horarioAberto =
+        (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]);
 
-const dataAgora = new Date();
-const diaAgora = dataAgora.getDay();
-const horarioAgora = dataAgora.getHours();
-const estaAberto = diasSemana.indexOf(diaAgora) !== -1;
-
-let horarioAberto =
-    (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]);
-
-if (estaAberto && horarioAberto){
-    funcionamento.classList.add('aberto');
+    if (estaAberto && horarioAberto) {
+        funcionamento.classList.add('aberto');
+    }
 }
 
