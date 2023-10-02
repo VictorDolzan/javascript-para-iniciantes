ad2e618464737c00db69cf43f9cd81c0a4379975
VictorDolsan <victorddf5@gmail.com>
2023-10-02 20:03:43 -0300

    Refactored scroll-animacao.js from function to class

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,20 +1,26 @@
-export default function animaScroll() {
-    const sections = document.querySelectorAll('[data-anime="scroll"]');
-    if (sections.length) {
-        const windowMetade = window.innerHeight * 0.6;
-        function initScroll() {
-            sections.forEach((section) => {
-                const sectionTop = section.getBoundingClientRect().top;
-                const isSectionVisible = (sectionTop - windowMetade) < 0
-                if (isSectionVisible) {
-                    section.classList.add('ativo');
-                }
-                else if (section.classList.contains('ativo')){
-                    section.classList.remove('ativo');
-                }
-            });
-        }
-        initScroll();
-        window.addEventListener('scroll', initScroll);
+export default class ScrollAnima {
+    constructor(sections) {
+        this.sections = document.querySelectorAll(sections);
+        this.windowMetade = window.innerHeight * 0.6;
+
+        this.animaScroll = this.animaScroll.bind(this);
+    }
+    animaScroll() {
+        this.sections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top;
+            const isSectionVisible = (sectionTop - this.windowMetade) < 0
+            if (isSectionVisible) {
+                section.classList.add('ativo');
+            }
+            else if (section.classList.contains('ativo')){
+                section.classList.remove('ativo');
+            }
+        });
+    }
+
+    init(){
+        this.animaScroll();
+        window.addEventListener('scroll', this.animaScroll);
+        return this;
     }
 }
